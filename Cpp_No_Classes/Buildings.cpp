7c7e32250f2cd4fcb56392c36df5829f40b8b544
Josh Smith <rvpjosh@yahoo.com>
2018-03-05 03:39:17 -0600

    Move Building code to Buildings own header and source files

---
+++ Cpp_No_Classes/Buildings.cpp
@@ -0,0 +1,79 @@
+#include "Buildings.h"
+#include "Units.h"
+
+// Building definitions
+namespace Game::Buildings
+{
+	template<Type t> _Building CreateBuilding( Team _team )
+	{
+		return _Building(
+			traits<t>::fire_resistance,
+			traits<t>::melee_resistance,
+			traits<t>::projectile_resistance,
+			traits<t>::width,
+			traits<t>::height,
+			traits<t>::max_hp,
+			_team,
+			t
+		);
+	}
+	_Building::_Building( Team _team, Type _type )
+	{
+		switch( _type )
+		{
+			case Buildings::Type::Hut:
+				*this = CreateBuilding<Type::Hut>( _team );
+				break;
+			case Buildings::Type::House:
+				*this = CreateBuilding<Type::House>( _team );
+				break;
+			case Buildings::Type::Mansion:
+				*this = CreateBuilding<Type::Mansion>( _team );
+				break;
+			case Buildings::Type::Tent:
+				*this = CreateBuilding<Type::Tent>( _team );
+				break;
+			case Buildings::Type::Butcher:
+				*this = CreateBuilding<Type::Butcher>( _team );
+				break;
+			case Buildings::Type::Market:
+				*this = CreateBuilding<Type::Market>( _team );
+				break;
+			case Buildings::Type::Warehouse:
+				*this = CreateBuilding<Type::Warehouse>( _team );
+				break;
+			case Buildings::Type::Barracks:
+				*this = CreateBuilding<Type::Barracks>( _team );
+				break;
+			case Buildings::Type::Stable:
+				*this = CreateBuilding<Type::Stable>( _team );
+				break;
+			case Buildings::Type::Port:
+				*this = CreateBuilding<Type::Port>( _team );
+				break;
+			default:
+				break;
+		}
+
+	}
+	_Building::_Building(
+		int32_t _fire_resistance,
+		int32_t _melee_resistance,
+		int32_t _projectile_resistance,
+		int32_t _width,
+		int32_t _height,
+		int32_t _hp,
+		Team _team,
+		Type _type )
+	{}
+
+	_Building::operator _Entity&( )
+	{
+		return entity;
+	}
+	_Building::operator const _Entity&( )const
+	{
+		return entity;
+	}
+
+}
---
+++ Cpp_No_Classes/Buildings.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Game
+{
+	namespace Buildings
+	{
+		enum class Type
+		{
+			Hut, House, Mansion, Tent,
+			Butcher, Market, Warehouse,
+			Barracks, Stable, Port
+		};
+
+		template<Type T> struct traits
+		{};
+		template<> struct traits<Type::Hut>
+		{
+			static constexpr int32_t fire_resistance = -10;
+			static constexpr int32_t melee_resistance = 25;
+			static constexpr int32_t projectile_resistance = 25;
+			static constexpr int32_t width = 2, height = 2;
+			static constexpr int32_t max_hp = 1000;
+		};
+		template<> struct traits<Type::House>
+		{
+			static constexpr int32_t fire_resistance = 10;
+			static constexpr int32_t melee_resistance = 100;
+			static constexpr int32_t projectile_resistance = 125;
+			static constexpr int32_t width = 3, height = 3;
+			static constexpr int32_t max_hp = 1500;
+		};
+		template<> struct traits<Type::Mansion>
+		{
+			static constexpr int32_t fire_resistance = 20;
+			static constexpr int32_t melee_resistance = 400;
+			static constexpr int32_t projectile_resistance = 500;
+			static constexpr int32_t width = 5, height = 5;
+			static constexpr int32_t max_hp = 2500;
+		};
+		template<> struct traits<Type::Tent>
+		{
+			static constexpr int32_t fire_resistance = 0;
+			static constexpr int32_t melee_resistance = 0;
+			static constexpr int32_t projectile_resistance = 0;
+			static constexpr int32_t width = 1, height = 1;
+			static constexpr int32_t max_hp = 500;
+		};
+		template<> struct traits<Type::Butcher>
+		{
+			static constexpr int32_t fire_resistance = 20;
+			static constexpr int32_t melee_resistance = 200;
+			static constexpr int32_t projectile_resistance = 500;
+			static constexpr int32_t width = 3, height = 3;
+			static constexpr int32_t max_hp = 2500;
+		};
+		template<> struct traits<Type::Market>
+		{
+			static constexpr int32_t fire_resistance = 20;
+			static constexpr int32_t melee_resistance = 40;
+			static constexpr int32_t projectile_resistance = 10;
+			static constexpr int32_t width = 5, height = 5;
+			static constexpr int32_t max_hp = 1500;
+		};
+		template<> struct traits<Type::Warehouse>
+		{
+			static constexpr int32_t fire_resistance = 100;
+			static constexpr int32_t melee_resistance = 600;
+			static constexpr int32_t projectile_resistance = 1000;
+			static constexpr int32_t width = 5, height = 5;
+			static constexpr int32_t max_hp = 3000;
+		};
+		template<> struct traits<Type::Barracks>
+		{
+			static constexpr int32_t fire_resistance = 100;
+			static constexpr int32_t melee_resistance = 1000;
+			static constexpr int32_t projectile_resistance = 1000;
+			static constexpr int32_t width = 5, height = 5;
+			static constexpr int32_t max_hp = 3000;
+		};
+		template<> struct traits<Type::Stable>
+		{
+			static constexpr int32_t fire_resistance = 50;
+			static constexpr int32_t melee_resistance = 1000;
+			static constexpr int32_t projectile_resistance = 200;
+			static constexpr int32_t width = 3, height = 3;
+			static constexpr int32_t max_hp = 2000;
+		};
+		template<> struct traits<Type::Port>
+		{
+			static constexpr int32_t fire_resistance = 100;
+			static constexpr int32_t melee_resistance = 2000;
+			static constexpr int32_t projectile_resistance = 1500;
+			static constexpr int32_t width = 3, height = 3;
+			static constexpr int32_t max_hp = 2000;
+		};
+
+
+		struct _Building
+		{
+			_Building() = default;
+			_Building( Team _team, Type _type );
+			_Building(
+				int32_t _fire_resistance,
+				int32_t _melee_resistance,
+				int32_t _projectile_resistance,
+				int32_t _width,
+				int32_t _height,
+				int32_t _hp,
+				Team _team,
+				Type _type );
+
+			operator _Entity&( );
+			operator const _Entity&( )const
+			{
+				return entity;
+			}
+
+			_Entity entity = { EntityCategory::Building };
+
+			Type type;
+			int32_t fire_resistance;
+			int32_t melee_resistance;
+			int32_t projectile_resistance;
+			int32_t width, height;
+			int32_t hp;
+		};
+	}
+
+}
